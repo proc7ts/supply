d887e30dbdd0a9320fcb2420c8e3a86ede0dccf7
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-08 14:05:23 +0700

    Optimize unexpected abort processing

--- src/impl/index.ts
+++ src/impl/index.ts
@@ -1,7 +1,5 @@
 export * from './cb-0.state';
 export * from './cb-1.state';
-export * from './cb-n.state';
-export * from './off';
 export * from './off.state';
 export * from './state';
 export * from './unexpected-abort';
