a9d8f5caf37483c5c2ab87959de03a7f74edde03
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-05-21 11:05:17 +0700

    Add supply abort support

---
+++ src/abort/abort-supply-by.ts
@@ -0,0 +1,43 @@
+import { SupplyPeer } from '../supply-peer.js';
+import { Supply } from '../supply.js';
+
+/**
+ * Aborts supply by given signal.
+ *
+ * Affects the supply provided by the target supply peer, if present. Creates new supply otherwise.
+ *
+ * If the given `signal` already aborted, then cuts off the supply with abort reason. Otherwise, cuts off the supply
+ * once an abort `signal` received.
+ *
+ * @param signal - The signal that aborts the supply.
+ * @param target - Target supply peer to abort.
+ *
+ * @returns Either existing `target` supply, or a new one.
+ */
+export function abortSupplyBy(signal: AbortSignal, target?: SupplyPeer): Supply {
+  const supply = target?.supply ?? new Supply();
+
+  if (signal.aborted) {
+    supply.off(signal.reason);
+  } else {
+
+    const onAbort = (): void => {
+      signal.removeEventListener('abort', onAbort);
+      supply.off(signal.reason);
+    };
+
+    signal.addEventListener('abort', onAbort);
+  }
+
+  return supply;
+}
+
+declare global {
+  export interface AbortSignal {
+    readonly reason?: unknown;
+  }
+
+  export interface AbortController {
+    abort(reason?: unknown): void;
+  }
+}
---
+++ src/abort/mod.ts
@@ -0,0 +1,1 @@
+export * from './abort-supply-by.js';
